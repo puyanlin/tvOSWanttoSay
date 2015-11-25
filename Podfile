68566ab234c031a5a8441011f53cc44572e548a6
Joseph Lin <puyan.tw@gmail.com>
2015-11-26 00:43:27 +0800

    add tableview controller

---
+++ Podfile
@@ -0,0 +1,15 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'Want to Say' do
+
+end
+
+target 'Want to SayTests' do
+
+end
+
+target 'Want to SayUITests' do
+
+end
+
--- Want to Say/ViewController.swift
+++ Want to Say/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imgTitle: UIImageView!
+    @IBOutlet var btnMore: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //self.navigationController?.navigationBarHidden = true
+        btnMore.alpha = 0
+        
+    }
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        UIView.animateWithDuration(3) { () -> Void in
+            self.imgTitle.frame = CGRect(x: 225, y: 225, width: self.view.bounds.width-450, height:self.view.bounds.height-450)
+            self.btnMore.alpha = 0.8
+        }
     }
 
     override func didReceiveMemoryWarning() {
